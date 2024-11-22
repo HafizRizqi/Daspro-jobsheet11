54e698c8525dd2092191a6fe8d04541353e635d3
HafizRizqi <HafizRizqi>
2024-11-22 20:35:15 +0700

    Percobaan 1

---
+++ Kafe11.java
@@ -0,0 +1,16 @@
+public class Kafe11 {
+    public static void menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("Latte - Rp 22,000");
+        System.out.println("Teh Tarik - Rp 12,000");
+        System.out.println("Roti Bakar - Rp 10,000");
+        System.out.println("Mie Goreng - Rp 18,000");
+        System.out.println("=========================");
+        System.out.println("Silahkan pilih menu yang anda inginkan");
+    }
+    public static void main(String[] args) {
+        menu();
+    }
+}
